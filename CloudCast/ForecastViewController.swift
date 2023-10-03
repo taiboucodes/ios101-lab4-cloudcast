33989f50dfc3e0bceac493594f088487074d845a
iamtai <taiboubalde101@gmail.com>
2023-10-02 22:43:35 -0400

    finished lab

--- CloudCast/ForecastViewController.swift
+++ CloudCast/ForecastViewController.swift
@@ -7,6 +7,12 @@
 
 import UIKit
 
+struct Location {
+  let name: String
+  let latitude: Double
+  let longitude: Double
+}
+
 class ForecastViewController: UIViewController {
   
   @IBOutlet weak var locationLabel: UILabel!
@@ -16,10 +22,18 @@ class ForecastViewController: UIViewController {
   @IBOutlet weak var windDirectionLabel: UILabel!
   @IBOutlet weak var dateLabel: UILabel!
   @IBOutlet weak var forecastImageView: UIImageView!
-  
+    
+private var locations = [Location]() // stores the different locations
+    private var selectedLocationIndex = 0
   override func viewDidLoad() {
     super.viewDidLoad()
     addGradient()
+              // Create a few locations to show the forecast for. Feel free to add your own custom location!
+              let sanJose = Location(name: "San Jose", latitude: 37.335480, longitude: -121.893028)
+              let manila = Location(name: "Manila", latitude: 12.8797, longitude: 121.7740)
+              let italy = Location(name: "Italy", latitude: 41.8719, longitude: 12.5674)
+              locations = [sanJose, manila, italy]
+      changeLocation(withLocationIndex: 0)
   }
   
   private func addGradient() {
@@ -31,13 +45,39 @@ class ForecastViewController: UIViewController {
     gradientLayer.endPoint = CGPoint(x: 0.5, y: 1)
     view.layer.insertSublayer(gradientLayer, at: 0)
   }
+    private func changeLocation(withLocationIndex locationIndex: Int) {
+        guard locationIndex < locations.count else { return }
+        let location = locations[locationIndex]
+        locationLabel.text = location.name
+        WeatherForecastService.fetchForecast(latitude: location.latitude, longitude: location.longitude) { forecast in
+            self.configure(with: forecast)
+        }
+    }
+    
+    private func configure(with forecast: CurrentWeatherForecast) {
+        forecastImageView.image = forecast.weatherCode.image
+        descriptionLabel.text = forecast.weatherCode.description
+        temperatureLabel.text = "\(forecast.temperature)"
+        windspeedLabel.text = "\(forecast.windSpeed) mph"
+        windDirectionLabel.text = "\(forecast.windDirection)°"
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "MMMM d, yyyy"
+        dateLabel.text = dateFormatter.string(from: Date())
+    }
+       
   
   @IBAction func didTapBackButton(_ sender: UIButton) {
+      selectedLocationIndex = max(0, selectedLocationIndex - 1) // make sure selectedLocationIndex is always >= 0
+      changeLocation(withLocationIndex: selectedLocationIndex)
     
   }
   
   @IBAction func didTapForwardButton(_ sender: UIButton) {
+      selectedLocationIndex = min(locations.count - 1, selectedLocationIndex + 1) // make sure selectedLocationIndex is always < locations.count
+      changeLocation(withLocationIndex: selectedLocationIndex)
     
   }
+    
+    
 }
 
